53706f27ec03c9522a44d60cff9fe1ba708e4a3a
Nicolás Escalante <nlante@gmail.com>
2015-01-19 11:59:53 -0300

    Update and rename osx.zsh to osx.sh

--- osx.sh
+++ osx.sh
@@ -1,14 +1,5 @@
-#!/bin/zsh
-
-# configuration
-NAME="Nicolas Escalante"
-EMAIL="nlante@gmail.com"
-
-# ssh
-ln -sfn $PWD/ssh/config ~/.ssh/config
-
 # install brew
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # cask for brew
 brew tap phinze/cask
